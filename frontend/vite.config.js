c67f23151bdf6bf8c6e69bfd0bd699cde1603eb7
DimaYsos <nosovsasa3@gmail.com>
2026-04-14 16:21:16 +0300

    viteconfig v2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["dimaysos-devsecops-eae5.twc1.net"],
     host: '0.0.0.0',
     port: 3000,
     proxy: {
